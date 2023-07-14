09475beafb94d62d46dd81f2d2dc49d6d28941c8
Exequiel <eportero421@gmail.com>
2023-07-14 12:19:57 -0300

    Logica lista Agregando lógica para la lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+type Item = {
+
+  nombre: string;
+  costo: string;
+
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  items: Item[] = [
+
+    {
+      nombre: 'Converse',
+      costo: '100$'
+    },
+
+    {
+      nombre: 'Adidas',
+      costo: '200$'
+    },
+
+    {
+      nombre: 'Nike',
+      costo: '300$'
+    }
+
+  ]
+
+  mostrarLista: boolean = true;
+
+  toggleLista() {
+
+    this.mostrarLista = !this.mostrarLista;
+
+  }
+
   title = 'proyecto-angular';
 }
